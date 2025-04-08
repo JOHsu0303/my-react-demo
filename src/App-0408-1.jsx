223549f007b0b82737c1bce6d7ca5ba80aae420e
JOHsu0303 <tinahsu0224@gmail.com>
2025-04-08 16:53:03 +0800

    0408-4

---
+++ src/App-0408-1.jsx
@@ -0,0 +1,10 @@
+function App() {
+    return (
+        <>
+        <h1>Hi, React</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit ratione labore sunt fugiat inventore eveniet suscipit quia odio commodi iste doloribus voluptas harum tempore quis id aliquam voluptates, dolorem eius?</p>
+        </>
+    )
+}
+
+export default App;
