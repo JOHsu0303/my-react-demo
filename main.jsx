75ef7921a6c16d517b04c648a1a3ece4b48cfc7e
JOHsu0303 <tinahsu0224@gmail.com>
2025-04-15 11:42:30 +0800

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './0415/App-0415-2'
+// import './index.css'
+import './style.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
