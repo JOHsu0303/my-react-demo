58e2a3e85dfd9437bb8ca3fd9f9a7275c588e6a0
JOHsu0303 <tinahsu0224@gmail.com>
2025-04-23 10:21:52 +0800

    0423-1

--- src/0422/components/TodoWrapper.jsx
+++ src/0422/components/TodoWrapper.jsx
@@ -6,14 +6,14 @@ import Todo from "./Todo"
 function TodoWrapper() {
     // 因為有N個todo，所以用陣列存放物件資料
     const [todos, setTodos] = useState([
-        { content: '停車費', id: Math.random(), isCompleted:false },
-        { content: '對發票', id: Math.random(), isCompleted:false },
+        { content: '停車費', id: Math.random(), isCompleted: false, isEdit: false },
+        { content: '對發票', id: Math.random(), isCompleted: false, isEdit: false },
     ])
 
     // 建立加入新的todo內容函式
     // 其餘運算子(...名稱)
     const addTodo = (newContent) => {
-        setTodos([...todos, { content: newContent, id: Math.random(), isCompleted:false }])
+        setTodos([...todos, { content: newContent, id: Math.random(), isCompleted: false, isEdit: false }])
     }
 
     // 建立刪除todo函式
@@ -37,13 +37,42 @@ function TodoWrapper() {
         }))
     }
 
+    // 建立切換isEdit屬性函式，傳給Todo元件使用
+    const toggleIsEdit = (id) => {
+        setTodos(todos.map((todo) => {
+            // not false => trun
+            // not trun => false
+            // ! => not
+            return todo.id === id ? { ...todo, isEdit: !todo.isEdit } : todo
+        }))
+    }
+
+    // 建立完成修改的函式
+    // 修改完成後，會異動兩個值
+    // content => 新的修改內容
+    // isEdit => 改回false
+    const editTodo = (id, newContent) => {
+        setTodos(todos.map((todo) => {
+            return todo.id === id
+                ? { ...todo, content: newContent, isEdit: false }
+                : todo
+        }))
+    }
+
     return (
         <div className="wrapper">
             <h1>待辦事項</h1>
             <CreateForm addTodo={addTodo} />
             {
                 todos.map((todo) => {
-                    return <Todo todo={todo} key={todo.id} delTodo={delTodo} toggleCompleted={toggleCompleted} />  /* 左邊的todo會在Todo.jsx中帶入 */
+                    return <Todo
+                        todo={todo}  /* 左邊的todo會在Todo.jsx中帶入 */
+                        key={todo.id}
+                        delTodo={delTodo}
+                        toggleCompleted={toggleCompleted}
+                        toggleIsEdit={toggleIsEdit}
+                        editTodo={editTodo}
+                    />
                 })
             }
         </div>
